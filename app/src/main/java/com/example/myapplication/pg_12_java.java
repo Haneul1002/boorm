e7d2352e40bf66fe6c08e86479543907b3f64df9
AiliartsuaL2 <AiliartsuaL2@gmail.com>
2022-05-19 14:46:36 +0900

    pg_12 commit _ jh

--- app/src/main/java/com/example/myapplication/pg_12_java.java
+++ app/src/main/java/com/example/myapplication/pg_12_java.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class pg_12_java extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pg_12);
     }
 }
