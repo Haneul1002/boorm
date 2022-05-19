561ecef2315d6343bb5d9e3a6c44cb1520b4ff92
AiliartsuaL2 <AiliartsuaL2@gmail.com>
2022-05-19 17:07:50 +0900

    pg_11~16_commit_jh

---
+++ app/src/main/java/com/example/myapplication/pg_11_java.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pg_11_java extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pg_11);
+    }
+}
