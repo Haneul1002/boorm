182a630e1aad771f54c26d22c72b2a4e3d0fed77
AiliartsuaL2 <AiliartsuaL2@gmail.com>
2022-05-30 17:19:11 +0900

    main_activity_movement

--- app/src/main/java/com/example/myapplication/activity_main.java
+++ app/src/main/java/com/example/myapplication/activity_main.java
@@ -1,15 +1,66 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_main extends AppCompatActivity {
+    ImageButton btn_home;
+    ImageButton btn_messenger;
+    ImageButton btn_profile;
+    ImageButton btn_store;
+    ImageButton btn_config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_home = findViewById(R.id.imageButton3);
+        btn_messenger = findViewById(R.id.imageButton4);
+        btn_profile= findViewById(R.id.imageButton6);
+        btn_store = findViewById(R.id.imageButton7);
+        btn_config = findViewById(R.id.imageButton5);
+
+        btn_home.setOnClickListener(new View.OnClickListener(){
+            @Override
+                public void onClick(View v){
+                    Intent intent = new Intent(getApplicationContext(),activity_main.class);
+                    startActivity(intent);
+                }
+            });
+        btn_messenger.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),pg_25_java.class);
+                startActivity(intent);
+            }
+        });
+        btn_profile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),page_my_profile.class);
+                startActivity(intent);
+            }
+        });
+        btn_store.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),page_market_main.class);
+                startActivity(intent);
+            }
+        });
+        btn_config.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),pg_14_java.class);
+                startActivity(intent);
+            }
+        });
     }
 }
