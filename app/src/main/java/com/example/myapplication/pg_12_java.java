7aa1b497bdd92c0b52274ea9dcc1a3de46faabd4
AiliartsuaL2 <AiliartsuaL2@gmail.com>
2022-05-30 16:02:07 +0900

    main

--- app/src/main/java/com/example/myapplication/pg_12_java.java
+++ app/src/main/java/com/example/myapplication/pg_12_java.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class pg_12_java extends AppCompatActivity {
-
+    Button btn_review;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_12);
+
+        btn_review = findViewById(R.id.button2);
+
+        btn_review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),pg_13_java.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myapplication/pg_13_java.java
+++ app/src/main/java/com/example/myapplication/pg_13_java.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class pg_13_java extends AppCompatActivity {
-
+    Button btn_complete ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_13);
+
+        btn_complete = findViewById(R.id.button3);
+
+        btn_complete.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
     }
 }
