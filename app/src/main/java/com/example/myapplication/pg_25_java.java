25ca2e6e1fac5b2046a8d8ce2327123eb94f3529
nanan1230 <64476686+nanan1230@users.noreply.github.com>
2022-06-08 12:27:30 +0900

    0608

--- app/src/main/java/com/example/myapplication/pg_25_java.java
+++ app/src/main/java/com/example/myapplication/pg_25_java.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class pg_25_java extends AppCompatActivity {
 
     ImageButton btn_back;
+    ImageButton imageButton15;
+    Button button23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,27 @@ public class pg_25_java extends AppCompatActivity {
         setContentView(R.layout.pg_25);
 
         btn_back = findViewById(R.id.imageButton16);
-
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        imageButton15 = findViewById(R.id.imageButton15);
+        imageButton15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), pg_27_java.class);
+                startActivity(intent);
+            }
+        });
+        button23 = findViewById(R.id.button23);
+        button23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), pg_25_1java.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/myapplication/pg_28_java.java
+++ app/src/main/java/com/example/myapplication/pg_28_java.java
@@ -1,7 +1,8 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
 
 public class pg_28_java extends AppCompatActivity {
@@ -10,5 +11,24 @@ public class pg_28_java extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pg_28);
+    }
+
+    public void btnOnClick1(View view) {
+        AlertDialog.Builder dig = new AlertDialog.Builder((pg_28_java.this));
+        dig.setTitle("(이벤트) 카풀 이용하고 스탬프 받자!");
+        dig.setMessage("카풀 이용할수록 혜택이 팡팡!");
+        dig.show();
+    }
+    public void btnOnClick2(View view) {
+        AlertDialog.Builder dig = new AlertDialog.Builder((pg_28_java.this));
+        dig.setTitle("매칭이 완료되었습니다!");
+        dig.setMessage("김근원님이 매칭을 수락하셨습니다.\n시간 : 04시 29분\n장소 : 도안동 800");
+        dig.show();
+    }
+    public void btnOnClick3(View view) {
+        AlertDialog.Builder dig = new AlertDialog.Builder((pg_28_java.this));
+        dig.setTitle("(공지사항) V1.5 업데이트 내역");
+        dig.setMessage("위치의 정확도를 높였습니다.");
+        dig.show();
     }
 }
