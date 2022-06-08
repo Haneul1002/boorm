e684d7f57ec23070d7de181ec299e44ac3715f63
sourcem249 <sourcem249@gmail.com>
2022-06-08 09:55:31 +0900

    메뉴바수정

--- app/src/main/java/com/example/myapplication/activity_main.java
+++ app/src/main/java/com/example/myapplication/activity_main.java
@@ -1,15 +1,20 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class activity_main extends AppCompatActivity {
+public class activity_main extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
 }
