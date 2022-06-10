a4bd68609a2e5919a0f333a348b9120a124c93d1
sourcem249 <sourcem249@gmail.com>
2022-06-10 15:20:57 +0900

    상단바 수정 및 마켓 연동

--- app/src/main/java/com/example/myapplication/page_market_history.java
+++ app/src/main/java/com/example/myapplication/page_market_history.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class page_market_history extends AppCompatActivity {
+import com.example.myapplication.databinding.ActivityPageMarketHistoryBinding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_market_history);
+public class page_market_history extends Fragment {
+    ActivityPageMarketHistoryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = ActivityPageMarketHistoryBinding.inflate(inflater, container, false);
+        final ImageButton Button = (ImageButton) binding.imageButton;
+
+        Button.setOnClickListener(new View.OnClickListener() { // 이미지 버튼 이벤트 정의
+            @Override
+            public void onClick(View v) { //클릭 했을경우
+                requireActivity().onBackPressed();
+            }
+        });
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/myapplication/page_market_main.java
+++ app/src/main/java/com/example/myapplication/page_market_main.java
@@ -2,18 +2,37 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import com.example.myapplication.databinding.ActivityPageMarketMainBinding;
+
 
 public class page_market_main extends Fragment {
+    ImageButton history, wishlist;
+    ActivityPageMarketMainBinding binding;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = ActivityPageMarketMainBinding.inflate(inflater, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_page_market_main, container, false);
+        wishlist = binding.wishlist;
+        history = binding.history;
+
+        history.setOnClickListener(view -> {
+            AppCompatActivity activity = (AppCompatActivity) view.getContext();
+            Fragment fragment = new page_market_history();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).addToBackStack(null).commit();
+        });
+
+        wishlist.setOnClickListener(view -> {
+            AppCompatActivity activity = (AppCompatActivity) view.getContext();
+            Fragment fragment = new page_market_wishlist();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).addToBackStack(null).commit();
+        });
+        return binding.getRoot();
     }
 }
